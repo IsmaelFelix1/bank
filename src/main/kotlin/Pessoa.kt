1b53501b5ba50a9953055419ed83dac409239654
Ismael Felix <ismael.duarte017@gmail.com>
2021-02-14 12:43:44 -0300

    updated to talk about class members

--- src/main/kotlin/Pessoa.kt
+++ src/main/kotlin/Pessoa.kt
@@ -4,12 +4,15 @@ class Pessoa {
     var cpf: String = "064.443.030.42"
     private set
 
+    constructor()
+
+    fun pessoaInfo() = "$name e $cpf"
+
+
 }
 
 fun main(){
     val p1 = Pessoa();
 
-    println(p1)
-    println(p1.name)
-    println(p1.cpf)
+    println(p1.pessoaInfo())
 }
