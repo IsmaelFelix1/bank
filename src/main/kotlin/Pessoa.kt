60595b7d93f51b4ada17885040ecce4c13dc8151
Ismael Felix <ismael.duarte017@gmail.com>
2021-02-14 12:40:10 -0300

    updated to apply the private modifier to set function of cpf proporty

--- src/main/kotlin/Pessoa.kt
+++ src/main/kotlin/Pessoa.kt
@@ -2,13 +2,14 @@ class Pessoa {
 
     var name: String = "Ismael"
     var cpf: String = "064.443.030.42"
-    var idade: Int = 20
+    private set
+
 }
 
 fun main(){
     val p1 = Pessoa();
 
+    println(p1)
     println(p1.name)
     println(p1.cpf)
-    println(p1.idade)
 }
