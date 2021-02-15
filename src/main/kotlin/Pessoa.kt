0a72c20c153fd4c18c9824f406ca1d08e3969e99
Ismael Felix <ismael.duarte017@gmail.com>
2021-02-15 11:30:50 -0300

    updated to make person and employee abstract class

--- src/main/kotlin/Pessoa.kt
+++ src/main/kotlin/Pessoa.kt
@@ -1,18 +1,4 @@
-class Pessoa {
-
-    var name: String = "Ismael"
-    var cpf: String = "064.443.030.42"
-    private set
-
-    constructor()
-
-    fun pessoaInfo() = "$name e $cpf"
-
-
-}
-
-fun main(){
-    val p1 = Pessoa();
-
-    println(p1.pessoaInfo())
-}
+abstract class Pessoa (
+     val nome: String,
+     val cpf: String
+        )
